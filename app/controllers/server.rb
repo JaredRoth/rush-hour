48c8b9d21b40185e6e41d3351b1fa2e2f302159e
Jared Roth <jared.roth@gmail.com>
2016-03-31 23:15:46 -0600

    Completely unfinished

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -9,38 +9,39 @@ module RushHour
     end
 
     post '/sources' do
-      client = Client.new(params[:client])
+      client = Client.new(params)
 
       if client.save
         status 200
         body "{'identifier':'#{client.identifier}'}"
 
-      elsif params[:client][:identifier].nil? || params[:client][:rootUrl].nil?
+      elsif params[:identifier].nil? || params[:rootUrl].nil?
         status 400
-        body "#{client.errors.full_messages.join(',')}"
+        body "#{client.errors.full_messages.join(', ')}"
 
       else #Client.find_by(:identifier => params[:client][:identifier])
         status 403
-        body "#{client.errors.full_messages.join(',')}"
+        body "#{client.errors.full_messages.join(', ')}"
       end
     end
 
-    post '/sources/:IDENTIFIER/data' do |identifier|
-      require 'pry'; binding.pry
-      parsed_payload = JSON.parse(params[:payload_request][:payload])
+    post '/sources/:identifier/data' do |identifier|
+      # parsed_payload = params[:payload] && params[:payload].length >= 2 ? JSON.parse(params[:payload], symbolize_names: true) : nil
+      # parsed_payload = JSON.parse(params[:payload], symbolize_names: true)
+      # parsed_payload = params[:payload]
 
-      ip                = Ip.find_or_create_by(:ip => params[:payload_request][:payload][:ip])
-      resolution        = Resolution.find_or_create_by(:resolution_width => params[:payload_request][:payload][:resolutionWidth], :resolution_height => params[:payload_request][:payload][:resolutionHeight])
-      user_agent_string = UserAgentString.find_or_create_by(:user_agent_os => UserAgent.parse(params[:payload_request][:payload][:userAgent]).platform, :user_agent_browser => UserAgent.parse(params[:payload_request][:payload][:userAgent]).browser)
-      event             = Event.find_or_create_by(:event_name => params[:payload_request][:payload][:eventName])
-      request_type      = RequestType.find_or_create_by(:request_type => params[:payload_request][:payload][:requestType])
-      referrer          = Referrer.find_or_create_by(:referred_by => params[:payload_request][:payload][:referredBy])
-      url               = Url.find_or_create_by(:url => params[:payload_request][:payload][:url])
-      client            = Client.find_or_create_by(:identifier => params[:payload_request][:payload][:identifier], :rootUrl => params[:payload_request][:payload][:rootUrl])
+      ip                = Ip.find_or_create_by(:ip => parsed_payload[:ip])
+      resolution        = Resolution.find_or_create_by(:resolution_width => parsed_payload[:resolutionWidth], :resolution_height => parsed_payload[:resolutionHeight])
+      user_agent_string = UserAgentString.find_or_create_by(:user_agent_os => UserAgent.parse(parsed_payload[:userAgent]).platform, :user_agent_browser => UserAgent.parse(parsed_payload[:userAgent]).browser)
+      event             = Event.find_or_create_by(:event_name => parsed_payload[:eventName])
+      request_type      = RequestType.find_or_create_by(:request_type => parsed_payload[:requestType])
+      referrer          = Referrer.find_or_create_by(:referred_by => parsed_payload[:referredBy])
+      url               = Url.find_or_create_by(:url => parsed_payload[:url])
+      client            = Client.find_or_create_by(:identifier => identifier, :rootUrl => parsed_payload[:referredBy])
 
-      PayloadRequest.new(
-      :requested_at         => params[:payload_request][:payload][:requestedAt],
-      :responded_in         => params[:payload_request][:payload][:respondedIn],
+      payload = PayloadRequest.new(
+      :requested_at         => parsed_payload[:requestedAt],
+      :responded_in         => parsed_payload[:respondedIn],
       :ip_id                => ip.id,
       :resolution_id        => resolution.id,
       :user_agent_string_id => user_agent_string.id,
@@ -51,21 +52,21 @@ module RushHour
       :client_id            => client.id
       )
 
-      if client.save
-        status 200
-        # body "{'identifier':'#{client.identifier}'}"
-
-      elsif params[:payload_request][:payload].nil?
+      if params[:payload].nil?
         status 400
-        body "#{client.errors.full_messages.join(',')}"
+        body "#{payload.errors.full_messages.join(', ')}"
 
-      elsif Client.where(identifier: identifier).any? == false
+      elsif Client.where(identifier: identifier).any?
         status 403
-        body "#{client.errors.full_messages.join(',')}"
+        body "#{payload.errors.full_messages.join(', ')}"
 
-      else #Client.find_by(:identifier => params[:client][:identifier])
+      elsif payload.save
+        status 200
+        # body "{'identifier':'#{payload.identifier}'}"
+
+      else
         status 403
-        body "#{client.errors.full_messages.join(',')}"
+        body "#{payload.errors.full_messages.join(', ')}"
       end
     end
   end
--- test/controllers/receive_payload_test.rb
+++ test/controllers/receive_payload_test.rb
@@ -9,38 +9,39 @@ class ReceivePayloadTest < Minitest::Test
   end
 
   def test_create_a_payload_with_valid_attributes
-    post '/sources', { client: { identifier: "yahoo", rootUrl: "http://yahoo.com" } }
-    post '/sources/yahoo/data', { payload_request: {payload:'{"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'}}
-    assert_equal 1, Client.count
+    skip
+    post '/sources', { identifier: "yahoo", rootUrl: "http://yahoo.com" }
+    post '/sources/yahoo/data', { payload: '{"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211" }' }
+    assert_equal 1, PayloadRequest.count
     assert_equal 200, last_response.status
     assert_equal "{'identifier':'yahoo'}", last_response.body
   end
 
   def test_cannot_create_a_payload_that_already_exists
     skip
-    post '/sources', { client: { identifier: "yahoo", rootUrl: "http://yahoo.com" } }
-    post '/sources/yahoo/data', { payload_request: 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}' }
-    assert_equal 1, Client.count
-    post '/sources/yahoo/data', { payload_request: 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}' }
-    assert_equal 1, Client.count
+    post '/sources', { identifier: "yahoo", rootUrl: "http://yahoo.com" }
+    post '/sources/yahoo/data', { payload: '{"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211" }' }
+    assert_equal 1, PayloadRequest.count
+    post '/sources/yahoo/data', { payload: '{"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211" }' }
+    assert_equal 1, PayloadRequest.count
     assert_equal 403, last_response.status
     assert_equal "Change this too", last_response.body
   end
 
   def test_cannot_create_a_payload_with_unknown_identifier
     skip
-    post '/sources', { client: { identifier: "yahoo", rootUrl: "http://yahoo.com" } }
-    post '/sources/jumpstart/data', { payload_request: 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}' }
-    assert_equal 0, Client.count
+    post '/sources', { identifier: "yahoo", rootUrl: "http://yahoo.com" }
+    post '/sources/jumpstart/data', { payload: '{"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211" }' }
+    assert_equal 0, PayloadRequest.count
     assert_equal 403, last_response.status
     assert_equal "Wrong ID", last_response.body
   end
 
-  def test_cannot_create_a_payload_with_missing_rootUrl
-    skip
-    post '/sources', { client: { identifier: "yahoo", rootUrl: "http://yahoo.com" } }
-    post '/sources/yahoo/data', { payload_request: {} }
-    assert_equal 0, Client.count
+  def test_cannot_create_a_payload_without_payload
+
+    post '/sources', { identifier: "yahoo", rootUrl: "http://yahoo.com" }
+    post '/sources/yahoo/data'
+    assert_equal 0, PayloadRequest.count
     assert_equal 400, last_response.status
     assert_equal "We Need to Change this", last_response.body
   end
