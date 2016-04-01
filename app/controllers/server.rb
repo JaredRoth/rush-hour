a4f3ccc9074bb3eba0013eb131a003da165350d6
Jared Roth <jared.roth@gmail.com>
2016-03-31 20:05:54 -0600

    Truly tests iteration 4

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -9,18 +9,18 @@ module RushHour
     end
 
     post '/sources' do
-      client = Client.new(params)
+      client = Client.new(params[:client])
 
       if client.save
         status 200
-        body "{\"identifier\":\"#{client.identifier}\"}"
+        body "{'identifier':'#{client.identifier}'}"
 
-      elsif Client.exists?(:identifier => params[:identifier])
-        status 403
+      elsif params[:client][:identifier].nil? || params[:client][:rootUrl].nil?
+        status 400
         body "#{client.errors.full_messages.join(',')}"
 
-      else #params[:identifier].nil? || params[:rootUrl].nil?
-        status 400
+      else #Client.find_by(:identifier => params[:client][:identifier])
+        status 403
         body "#{client.errors.full_messages.join(',')}"
       end
     end
---
+++ test/controllers/create_client_test.rb
@@ -0,0 +1,40 @@
+require './test/test_helper'
+
+class CreateClientTest < Minitest::Test
+  include Rack::Test::Methods
+  include TestHelpers
+
+  def app
+    RushHour::Server
+  end
+
+  def test_create_a_client_with_valid_attributes
+    post '/sources', { client: { identifier: "yahoo", rootUrl: "http://yahoo.com" } }
+    assert_equal 1, Client.count
+    assert_equal 200, last_response.status
+    assert_equal "{'identifier':'yahoo'}", last_response.body
+  end
+
+  def test_cannot_create_client_that_already_exists
+    post '/sources', { client: { identifier: "yahoo", rootUrl: "http://yahoo.com" } }
+    assert_equal 1, Client.count
+    post '/sources', { client: { identifier: "yahoo", rootUrl: "http://yahoo.com" } }
+    assert_equal 1, Client.count
+    assert_equal 403, last_response.status
+    assert_equal "Identifier has already been taken", last_response.body
+  end
+
+  def test_cannot_create_a_client_with_missing_identifier
+    post '/sources', { client: { rootUrl: "http://yahoo.com" } }
+    assert_equal 0, Client.count
+    assert_equal 400, last_response.status
+    assert_equal "Identifier can't be blank", last_response.body
+  end
+
+  def test_cannot_create_a_client_with_missing_rootUrl
+    post '/sources', { client: { identifier: "yahoo" } }
+    assert_equal 0, Client.count
+    assert_equal 400, last_response.status
+    assert_equal "Rooturl can't be blank", last_response.body
+  end
+end
